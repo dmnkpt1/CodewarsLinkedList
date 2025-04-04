26c2e12fcc0d55d3184e3a0d8294f7b70bdf19cb
dmnkpt1 <petrus.pn@ucu.edu.ua>
2025-04-04 18:42:43 +0300

    Added solution for 'Linked Lists - Get Nth Node

--- Linked Lists - Get Nth Node/result4.py
+++ Linked Lists - Get Nth Node/result4.py
@@ -1,1 +1,17 @@
+from preloaded import Node
+
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+ 
+def get_nth(node, index):
+    nodes = []
+    while node is not None:
+        nodes.append(node)
+        node = node.next
+    if  index < 0 or len(nodes) <= index:
+        raise IndexError
+    return nodes[index]
 
