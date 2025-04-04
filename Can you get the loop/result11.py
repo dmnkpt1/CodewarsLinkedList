163f1589d1fb76dcb11d59d2a384eaeed6c5920b
dmnkpt1 <petrus.pn@ucu.edu.ua>
2025-04-04 20:11:49 +0300

    Added solution for Can you get the loop

--- Can you get the loop/result11.py
+++ Can you get the loop/result11.py
@@ -1,1 +1,12 @@
+def loop_size(node):
+    visited = {}
+    index = 0
 
+    while True:
+        if node not in visited:
+            visited[node] = index
+            node = node.next
+            index += 1
+        else:
+            start_index = visited[node]
+            return index - start_index
