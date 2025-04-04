c0c7acd3b5d451053b549ac5d6c1793979c670ee
dmnkpt1 <petrus.pn@ucu.edu.ua>
2025-04-04 18:37:06 +0300

    Added solution for Parse a linked list from a string

--- Parse a linked list from a string/result2.py
+++ Parse a linked list from a string/result2.py
@@ -1,1 +1,21 @@
+class Node():
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
 
+
+def linked_list_from_string(s):
+    if not isinstance(s,str):
+        return 'None'
+    head = None
+#     for i in range(len(s))[::-1][1:]:
+#         new = Node(s[i])
+#         new.next = head
+#         head = new
+#     return head
+    elements = s.split(" -> ")
+    elements = elements[:-1]
+    for value in elements[::-1]:
+        head = Node(int(value), head)
+
+    return head
